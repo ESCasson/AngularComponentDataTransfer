814ff2e09cdb25e59a84033fb170650b99b9740c
Eleanor Casson <cassona08@gmail.com>
2021-03-05 12:03:47 +0000

    adds simple get request

---
+++ src/app/api.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+  API_KEY = 'd1af742747bc4be1b1e81922e8c5014c';
+
+  constructor(private httpClient: HttpClient) { }
+
+  public getNews(){
+    return this.httpClient.get(`https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=${this.API_KEY}`);
+  }
+}
